a6c9f3e3ca9be917b60bf66f13f0424087fab3d6
Christian <72566246+Christian-Carrizales@users.noreply.github.com>
2020-11-03 23:33:17 -0600

    Add files via upload

---
+++ Mod 5 Example Problem.py
@@ -0,0 +1,8 @@
+#Module 5 Lab Activity
+#Programmer: Christian Carrizales
+#Date: Tuesday, November 3, 2020
+#At the top of this file: Module 5 Lab Activity.
+#Description: Print Hello World a 100 times Example Problems.
+n=10
+while n!=0:
+    print("Hello World")
