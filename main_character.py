897f0b7701bf95b9d2a2ad7714ff3809c98e2621
Christian <72566246+Christian-Carrizales@users.noreply.github.com>
2020-12-10 02:19:51 -0600

    Add files via upload

---
+++ main_character.py
@@ -0,0 +1,18 @@
+
+#The main character will have the following variables, all initially set to empty values
+def main_character():
+    name = "Bosa"
+    age = 20
+    inventory = []
+    inventory.append("Spray Cans")
+    inventory.append("Map")
+    inventory.append("BookBag")
+    print(inventory)
+    inventory.remove("spray can")
+    print(inventory)
+
+    Health = [100]
+               
+
+
+        
